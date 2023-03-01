30f5d63bec6544aed0a9379d673a6fe048d962b8
cmadrid48 <cmadrid48@gmail.com>
2023-02-28 20:14:37 -0800

    added shape scripts, and classes

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+//pulling all required npm and sourced scripts
 const inquirer = require('inquirer');
 const fs = require('fs');
 const {generateSvg} = require('./lib/generateSvg');
 const {createShape} = require('./lib/createShape');
 
+//inquirer user questions to be used in project, this will then direct the new logo where to go
 inquirer
     .prompt([
         {
@@ -26,13 +28,15 @@ inquirer
             message: `Please choose logo shape`,
             choices: ['triangle', 'circle', 'square'],
         },
-    ])
-    .then((data) => {
-        const svgPath = './examples/circle.svg';
-        const finalLogo = createShape(data);
+])
 
-        //Creates the svg logo
-        fs.writeFile(svgPath, generateSvg(finalLogo), (err) => 
-        err ? console.error(err) : console.log('Generated logo.svg'));
-    })
-    .catch((err) => console.log(err));
+.then((data) => {
+    const svgPath = './examples/circle.svg';
+    const finalLogo = createShape(data);
+
+    //Creates the svg logo
+    fs.writeFile(svgPath, generateSvg(finalLogo), (err) => 
+    err ? console.error(err) : console.log('Generated logo.svg'));
+})
+
+.catch((err) => console.log(err));
